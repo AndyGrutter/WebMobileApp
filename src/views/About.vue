c2feede51bf08c4bc6a9d0e90406bc319e291e83
Andy Grütter <andy.grutter@gmail.com>
2021-11-17 10:14:03 +0100

    init
    init

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an about page thingy</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Commute person="Andy" start="Earth" destination="Mars"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Commute from '@/components/Commute.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Commute
+  },
+  data: function() {
+    return {
+      person: "Andy",
+      start: "Earth",
+      destination: "Mars"
+    }
+  }
+}
+</script>
